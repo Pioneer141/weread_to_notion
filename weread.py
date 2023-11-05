8b6b4b4e7d154dcc0bf73730f9978c8899d7d6ae
Pioneer-117 <w982125792@gmail.com>
2023-11-06 01:53:05 +0800

    Update weread.py
    将sort转化为标准北京时间

--- weread.py
+++ weread.py
@@ -207,7 +207,7 @@ def insert_to_notion(bookName, bookId, cover, sort, author,isbn,rating):
         "ISBN": {"rich_text": [{"type": "text", "text": {"content": isbn}}]},
         "URL": {"url": f"https://weread.qq.com/web/reader/{calculate_book_str_id(bookId)}"},
         "Author": {"rich_text": [{"type": "text", "text": {"content": author}}]},
-        "Sort": {"number": sort},
+        "Sort": {"date": {"start": datetime.utcfromtimestamp(sort)).strftime("%Y-%m-%d %H:%M:%S"), "time_zone": "Asia/Shanghai"}},
         "Rating": {"number": rating},
         "Cover": {"files": [{"type": "external", "name": "Cover", "external": {"url": cover}}]},
     }
