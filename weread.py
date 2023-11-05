1416afdee553f6dc7cc6812bbb5e4419152701df
Pioneer-117 <w982125792@gmail.com>
2023-11-06 03:03:40 +0800

    Update weread.py

--- weread.py
+++ weread.py
@@ -210,9 +210,10 @@ def insert_to_notion(bookName, bookId, cover, sort, author,isbn,rating):
         "Sort": {"number": sort},
         "Rating": {"number": rating},
         "Cover": {"files": [{"type": "external", "name": "Cover", "external": {"url": cover}}]},
+        "Latest": {"date": [{"type": "date","name": "Cover", "start": datetime.utcfromtimestamp(sort).strftime("%Y-%m-%d %H:%M:%S"), "time_zone": "Asia/Shanghai"}]},
     }
 
-    properties["Latest"] = {"date": {"start": datetime.utcfromtimestamp(sort).strftime("%Y-%m-%d %H:%M:%S"), "time_zone": "Asia/Shanghai"}}
+    # properties["Latest"] = {"date": {"start": datetime.utcfromtimestamp(sort).strftime("%Y-%m-%d %H:%M:%S"), "time_zone": "Asia/Shanghai"}}
 
     read_info = get_read_info(bookId=bookId)
     if read_info != None:
